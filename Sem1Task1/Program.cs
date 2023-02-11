687f00b6588f2553c84b83890c8abaefc0c58385
Ksenia Lapshina <ksusha-lap1985@yandex.ru>
2023-02-11 23:35:13 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №1
+// Напишите программу, которая на вход принимает
+// два числа и проверяет, является ли первое число квадратом второго
+
+Console.WriteLine("Введите число 1");
+var number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+var number2 = Convert.ToDouble(Console.ReadLine());
+
+if (number1 == Math.Pow(number2, 2))
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+else
+    Console.WriteLine($"Число {number1} НЕ является квадратом числа {number2}");
+
+
