c6c313afb6ef485c2b4042fa047ed629a7250d44
Ksenia Lapshina <ksusha-lap1985@yandex.ru>
2023-02-17 22:18:00 +0300

    закомитила task3

--- Sem1Task3/Program.cs
+++ Sem1Task3/Program.cs
@@ -7,15 +7,15 @@ string? lineDay = Console.ReadLine();
 if (lineDay != null)
 {
     int day = int.Parse(lineDay);
-    // string[] dayWeek = new string[7];
-    // dayWeek[0] = "Понедельник";
-    // dayWeek[1] = "Вторник";
-    // dayWeek[2] = "Среда";
-    // dayWeek[3] = "Четверг";
-    // dayWeek[4] = "Пятница";
-    // dayWeek[5] = "Суббота";
-    // dayWeek[6] = "Воскресенье";
-    // Console.WriteLine(dayWeek[day - 1]);
-    string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-Ru").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(day));
-    Console.WriteLine(outDayOfWeek);
+    string[] dayWeek = new string[7];
+    dayWeek[0] = "Понедельник";
+    dayWeek[1] = "Вторник";
+    dayWeek[2] = "Среда";
+    dayWeek[3] = "Четверг";
+    dayWeek[4] = "Пятница";
+    dayWeek[5] = "Суббота";
+    dayWeek[6] = "Воскресенье";
+    Console.WriteLine(dayWeek[day - 1]);
+    //string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-Ru").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(day));
+    Console.WriteLine(dayWeek);
 }
