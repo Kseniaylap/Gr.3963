8d74dceb50fafe2e7e57e414016a5782b607d9b2
Ksenia Lapshina <ksusha-lap1985@yandex.ru>
2023-02-21 18:48:18 +0300

    Добавила задачи 10,13,14,15,16

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,37 @@
+﻿// №13 Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// * Сделать вариант для числа длиной до 10 цифр не используя char или string
+
+// Console.Write("Введите число: ");
+// // int number = int.Parse(Console.ReadLine()??"0");
+// int GetThirdRank(int number);
+// if (number < 100)
+// {
+//     Console.WriteLine("Введенное число не содержит третьей цифры ");
+// }
+
+// while (number > 999)
+// {
+//     Console.WriteLine(number/=10);
+// }
+//     return number % 10;
+Console.WriteLine("Введите число ");
+// Вводим локальную переменную
+string? num = Console.ReadLine();
+// создаем условие чтобы исключить введение числа менее трех знаков
+if (num != null)
+{
+if (num.Length >= 3)
+// создаем массив, выводим третье число массива
+{
+    char[]array = num.ToCharArray();
+    Console.WriteLine(array[2]);
+}    
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+}
---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №16
+// Напишите программу, которая принимает на вход
+// два числа и проверяет, является ли одно число квадратом другого.
+// Например:
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+Console.WriteLine("Введите первое число" );
+int numA = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите второе число" );
+int numB = int.Parse(Console.ReadLine()??"0");
+void squareTest(int num1, int num2)
+{
+    if (num1 == num2 * num2)
+    {
+        Console.WriteLine($"Первое число {num1} квадрат второго числа {num2}");  
+    }
+    else
+    {
+        Console.WriteLine($"Первое число {num1} не квадрат второго числа {num2}");
+    }
+}
+
+squareTest(numA, numB);
+squareTest(numB, numA);
+
+
+
