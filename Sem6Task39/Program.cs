661ae48b40094ae11d9df1e03679069ff50429dc
Ksenia Lapshina <ksusha-lap1985@yandex.ru>
2023-03-14 11:32:22 +0300

    Add sem 6 tasks 39,40,41,42,43

---
+++ Sem6Task39/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача №39
+// Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на
+// первом месте, а первый - на последнем и т.д.)
+// Например:
+// [1 2 3 4 5] -> [5 4 3 2 1] [6 7 3 6] -> [6 3 7 6]
+// Комментарий: эту задачу можно решить 2 способами: 1)
+// менять числа местами в исходном массиве; 2) создать
+// новый массив и в него записать перевёрнутый исходный
+// массив по элементам.
+int[] Gen1DArr(int len, int min, int max)
+{
+    int [] arr = new int [len];
+    for (int i = 0; i < len; i++) 
+    arr [i]= new Random().Next(min, max + 1);
+    return arr;
+}
+void Print1DArr(int[]arr)
+{
+    Console.Write("[");
+    for (int i = 0;i<arr.Length - 1;i++) 
+    Console.Write(arr[i]+","); 
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+void SwapArray(int[]arr)
+{
+    int bufElem = 0;
+    for (int i = 0;i<arr.Length/2;i++)
+    {
+        bufElem = arr[i];
+        arr[i]=arr[arr.Length-1-i];
+        arr[arr.Length-1-i]=bufElem;
+    }
+}
+int [] testArray = Gen1DArr(123,10,100);
+Print1DArr(testArray);
+SwapArray(testArray);
+Print1DArr(testArray);
---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+// * Пользователь вводит число нажатий, затем программа следит за нажатиями и
+// выдает сколько чисел больше 0 было введено.
+int ReadData(string masg)
+{
+    Console.WriteLine(masg);//выводит сообщение
+    string line = Console.ReadLine();
+    int result = Convert.ToInt32(line);//считывает число
+    return result;// возвращает значение
+}
+int [] inputArr(int len)
+{
+    int [] array = new int[len];
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = ReadData($"Введите {i + 1}-й элемент");
+    }
+    return array;
+}
+void PrintData(int [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+int CountNum (int [] array)
+{
+    int res = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+       if(array[i]>0)
+       {
+        res++;
+       }
+    }
+    return res;
+}
+int len = ReadData("Введите количество элементов >");
+int[]array;
+array = inputArr(len);
+PrintData(array);
+Console.WriteLine($"Количество чисел больше 0 - {CountNum(array)}");
